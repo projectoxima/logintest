8d53aa8512e156b5caf8f792a5a49fe146c88f13
taufikoxima <taufik.oxima@gmail.com>
2014-11-17 10:35:36 +0700

    login test

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','datatables');
+$autoload['libraries'] = array('database','session');
 
 
 /*
